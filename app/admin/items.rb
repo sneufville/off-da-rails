686fc6486fda13a5d5645006e2e46d0c89f53b2a
sneufville <sneufville@academic.rrc.ca>
2024-07-10 13:25:57 -0500

    update permit_params for items such that items can be created and edited via active admin

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 ActiveAdmin.register Item do
-  permit_params :item_name, :item_category
+  permit_params :item_name, :item_category_id, :item_description, :item_cost
 end
